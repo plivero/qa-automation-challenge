bb2106ee459996316a2ca0860f135d13721058d6
Pedro <pedro_livero@hotmail.com>
2025-08-02 15:20:22 -0400

    feat: add basic UI test to validate homepage logo visibility

---
+++ cypress/e2e/specs/test1.cy.js
@@ -0,0 +1,10 @@
+import { HomePage } from "../pages/homePage";
+
+const home = new HomePage();
+
+describe("Home Page", () => {
+  it("should load the homepage and display the logo", () => {
+    home.visit();
+    home.getLogo().should("be.visible");
+  });
+});
